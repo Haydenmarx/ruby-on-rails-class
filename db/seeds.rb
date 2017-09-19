7008f7cb0d1a6af9addabed72d1f20119b773f3d
Hayden <haydenmarx@gmail.com>
2017-09-19 02:44:17 +0000

    >Updated Data Management: - Created database relationships. - Added custom scopes. - Added concern to enter web address for filler images. - Updated seeds file - Implemented nested attributes forms.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+3.times do |topic|
+    Topic.create!(
+        title: "Topic #{topic}"
+    )    
+end
+puts "3 topics created."
 
 10.times do |blog|
     Blog.create!(
         title: "My Blog Post #{blog}",
-	    body: "Hello, it's me. Hello, it's me. Hello, it's me. Hello, it's me. Hello, it's me. Hello, it's me. Hello, it's me. Hello, it's me. Hello, it's me. Hello, it's me. Hello, it's me. Hello, it's me. Hello, it's me. Hello, it's me. Hello, it's me. Hello, it's me. Hello, it's me. Hello, it's me. Hello, it's me. Hello, it's me."
+	    body: "Hello, it's me. Hello, it's me. Hello, it's me. Hello, it's me. Hello, it's me. Hello, it's me. Hello, it's me. Hello, it's me. Hello, it's me. Hello, it's me. Hello, it's me. Hello, it's me. Hello, it's me. Hello, it's me. Hello, it's me. Hello, it's me. Hello, it's me. Hello, it's me. Hello, it's me. Hello, it's me.", topic_id: Topic.last.id
     )
 end
-
 puts "10 blog posts created."
 
 5.times do |skill|
@@ -21,17 +26,33 @@ puts "10 blog posts created."
 	    percent_utilized: 15
     )
 end
-
 puts "5 skills created."
 
-9.times do |portfolio_item|
+1.times do |portfolio_item|
     Portfolio.create!(
-        title: "Portfolio title: #{portfolio_item}",
-    	subtitle: "Work I did",
+        title: "Portfolio title: #{portfolio_item + 1}",
+    	subtitle: "Ruby on Rails",
     	body: "Put some text here later. Don't forget to put some text here later.",
     	main_image: "https://placehold.it/600x400",
     	thumb_image: "https://placehold.it/350x200"
     )
 end
+puts "1 portfolio item created."
 
-puts "9 portfolio items created."
+8.times do |portfolio_item|
+    Portfolio.create!(
+        title: "Portfolio title: #{portfolio_item + 2}",
+    	subtitle: "HTML/CSS",
+    	body: "Put some text here later. Don't forget to put some text here later.",
+    	main_image: "https://placehold.it/600x400",
+    	thumb_image: "https://placehold.it/350x200"
+    )
+end
+puts "8 portfolio items created."
+
+3.times do |technology|
+    Portfolio.last.technologies.create!(
+        name: "Technology #{technology}"
+    )
+end
+puts "3 Technologies created."
